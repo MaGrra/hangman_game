21d72e7222ace9160d11280ea89f04d6049801af
MaGrra <mrti.grra@gmail.com>
2023-04-12 12:55:49 +0300

    Correct guess appears in progress
    Loop not working
    Input does not take all letters

--- lib/game.rb
+++ lib/game.rb
@@ -15,32 +15,48 @@ end
 end
 
 class Game
-    attr_accessor :guesses_left, :made_guesses, :word_progress
+    attr_accessor :guesses_left, :made_guesses, :word_progress, :guess
     attr_reader :word
    def initialize
     @word = Words.new
     @guesses_left = 9
     @made_guesses = []
     @word_progress = Array.new(@word.hidden.length, "_")
+    @guess = ""
    end
 
 def play
     p @word.hidden
     puts "Hello there! The word you need to guess is selected! It has #{@word.hidden.length} letters"
     #loop
+    current_guess = ""
     puts "Enter your chosen letter! You can still make #{@guesses_left} mistakes."
     valid_input(player_input)
+    check_letters
+    p @guess
+end
+
+def check_letters
+    if @word.hidden.include?(@guess)
+        @word_progress.each_with_index do | letter, index |
+            if @guess == @word.hidden[index]
+                @word_progress[index] = @word.hidden[index]
+            
+        end
+        end
+    else 
+        print "stil works but different"
+    end
     display_word
-    puts "Already made guesses: #{@made_guesses}"
-    puts "Progress: #{@word_progress.join(" ")}"
 end
 
 def display_word
-
+    puts "Already made guesses: #{@made_guesses}"
+    puts "Progress: #{@word_progress.join(" ")}"
 end 
     
 def player_input
-    gets.chop.to_s
+    @guess = gets.chop.to_s
 end
 
 def valid_input(players_guess)
